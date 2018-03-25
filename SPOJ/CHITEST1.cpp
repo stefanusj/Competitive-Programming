b88e6b1bb41e869063086fbb4d600cae687c881b
riskimidiw <riskimidiw@gmail.com>
2018-03-25 11:45:20 +0700

    Add CHITEST1, SMPCPH1

---
+++ SPOJ/CHITEST1.cpp
@@ -0,0 +1,20 @@
+// Status: Accepted (riskimidiw)
+//Problem : http://www.spoj.com/problems/CHITEST1/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// your code here
+	int x;
+	double a, b;
+	
+	cin >> x;
+	for(int i=0; i<x; i++) {
+		cin >> a >> b;
+		cout << a+b << endl;
+	}
+    
+	return 0;
+}
---
+++ SPOJ/SMPCPH1.cpp
@@ -0,0 +1,39 @@
+// Status: Accepted (riskimidiw)
+//Problem : http://www.spoj.com/problems/SMPCPH1/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// your code here
+    int n, m;
+    string key, cipher[100];
+    
+    cin >> n; cin.ignore();
+    getline(cin, key);
+    
+    cin >> m; cin.ignore();
+    for(int i=0; i<m; i++) {
+        getline(cin, cipher[i]);
+        for(int j=0; j<cipher[i].length(); j++) {
+            for(int k=0; k<key.length(); k++) {
+                if(cipher[i][j] == key[k]) {
+                    int t;
+                    if(k+1<=(n-1)) {
+                        t = k + 1;
+                        cipher[i][j] = key[t];
+                        break;
+                    } else{
+                        t = k % (n-1);
+                        cipher[i][j] = key[t];
+                        break;
+                    }
+                }
+            }
+        }
+        cout << cipher[i] << endl;
+    }
+
+	return 0;
+}
